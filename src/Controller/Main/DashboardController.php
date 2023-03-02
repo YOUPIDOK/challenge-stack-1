0b8825c5cc2e206eabeeb59ed211b393461c15c6
YOUPIDOK <58979097+YOUPIDOK@users.noreply.github.com>
2023-03-02 08:09:18 +0100

    Fix

--- src/Controller/Main/DashboardController.php
+++ src/Controller/Main/DashboardController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller\Main;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[IsGranted('ROLE_CLIENT')]
 class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'dashboard')]
